f00b1631ffc40e5bfcbb2ca836e8815274b31ccd
hortuadev <hortuadev@gmail.com>
2025-12-12 15:53:21 +0000

    feat(LoginStyles): creados los estilos del componente login

---
+++ utils/styles/LoginStyles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 28,
+    textAlign: "center",
+    marginBottom: 25,
+  },
+  input: {
+    borderWidth: 1,
+    padding: 12,
+    marginBottom: 15,
+    borderRadius: 6,
+  },
+  link: {
+    marginTop: 20,
+    textAlign: "center",
+    color: "blue",
+  },
+});
